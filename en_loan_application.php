a5ecddd97b488eda71dd0d24a07639e48db3ad78
Rinto Sarkar <rinto.sp@gmail.com>
2014-11-23 18:57:42 +0600

    My second commit

---
+++ en_loan_application.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loan Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<style type="text/css">
+	*{
+		margin: 0 auto;
+	}
+	.language{
+		background: url(http://i62.tinypic.com/15xvbd5.png) no-repeat 96% 0;
+		height: 29px;
+	  	overflow: hidden;
+	  	width: 240px;
+	  	float: left;
+	  	margin-top: 30px;
+	}
+
+	.language select {
+	   background: transparent;
+	   border: none;
+	   font-size: 14px;
+	   height: 29px;
+	   padding: 5px; /* If you add too much padding here, the options won't show in IE */
+	   width: 268px;
+	}
+
+	.language.slate {
+	   background: url(http://i62.tinypic.com/2e3ybe1.jpg) no-repeat right center;
+	   height: 34px;
+	   width: 240px;
+	}
+
+	.language.slate select {
+	   border: 1px solid #ccc;
+	   font-size: 16px;
+	   height: 34px;
+	   width: 268px;
+	}
+	.header_text{
+		float: left;
+		
+	}
+
+	.header_text h1{
+		text-align: center;
+		font-family: 'Comic Sans MS', cursive;
+		color: blue;
+		padding-left: 80px;
+		padding-right: 80px;
+		margin-top: 25px;
+		margin-bottom: 30px;
+	}
+
+	.hr-menu{
+		width: 100%;
+		height: 40px;
+		background-color: #000078;
+		z-index: 200px;
+		float: left;
+	}
+	.menus{
+		margin-left: 250px;
+		padding-top: 7px;
+		color: yellow;
+	}
+
+	.menus a{
+		padding-right: 10px;
+	}
+
+	a{
+		text-decoration:none;
+		font-size: 20px;
+		color: white;
+	}
+
+	a:hover{
+		color:yellow;
+		
+	}
+	a:active{
+		color:gray;
+	}
+	</style>
+</head>
+<body>
+<!-- header start -->
+	<div class="header">
+		<div class="row">
+			<div class="language slate">
+				<select>
+					<option>English</option>
+					<option>Français</option>
+					<option>Español</option>
+					<option>Deutsch</option>
+					<option>Português</option>
+				</select>
+			</div>
+			<div class="header_text">
+				<h1>OFFERING LOAN TOTALLY FREE OF CHARGE AND FEES</h1>
+			</div>
+		</div> <!-- Header row -->
+	</div> <!-- header div -->
+<!-- header end -->
+<!--horiozntal menu start-->
+	<div class="hr-menu">
+		<div class="menus">
+			<a href="#">Home | </a>
+			<a href="#">Careears | </a>
+			<a href="#">Contacts | </a>
+			<a href="#">Faq's</a>
+		</div>
+	</div>
+<!--horiozntal menu end-->
+<!--Vertical menu start-->
+	<div class="vr-menu">
+		<ul>
+			<li>Profile</li>
+			<li>Company Structure</li>
+			<li>Our Service</li>
+			<li>Job Descriptions</li>
+			<li>Loan Amount & Repayment</li>
+			<li>Loan Application</li>
+		</ul>
+	</div>
+<!--Vertical menu End-->
+
+<div class="container">
+		
+	</div> <!-- this is container div -->
+</body>
+</html>
